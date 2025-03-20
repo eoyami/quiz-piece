66dcb4457d27277bf33909acb286e60e691bf256
Yami <raphaelribeirocaetano@gmail.com>
2025-03-20 02:47:48 -0300

    add condicional renders and logic

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Yamato from '../../../public/assets/question_yamato.png'
 import type { Session } from "next-auth";
@@ -13,6 +13,8 @@ const GamePage = () => {
     setUserSession(session);
   }, [session]);
 
+
+  const [gaming, setGaming] = useState<boolean>(false)
 
   class counttimer {
     private tempoRestante: number;
@@ -64,26 +66,42 @@ const GamePage = () => {
 
 
   }
+
+
+  const handleGameStart = () => {
+    setGaming(true)
+    counttimerGame.start()
+  }
   const counttimerGame = new counttimer(15)
   const [timer, setTimer] = useState<number>(counttimerGame.getTempoRestante())
 
   return (
     <>
-    {userSession ? (
+
+    {gaming === false ? <button className="bg-green-500 text-black" onClick={() => {handleGameStart()}}>Começar a jogar</button> : null}
+    {userSession && gaming ? (
       <section className="flex flex-col w-screen h-screen justify-center items-center bg-neutral-950 max-sm:py-6 max-sm:px-3">
       <div className="flex flex-col justify-center items-center text-2xl">
         {userSession && (
           <>
-          <button onClick={() => {counttimerGame.start()}}>começar</button>
-          {timer > 0 ? (<div>Timer: {timer}</div>) : (<div>Tempo acabou</div>) }
+          {timer > 0 ? (<>
+            <div>Tempo restante: {timer}</div>
           <div className="flex">
             
-            <div className="flex flex-col bg-white text-gray-900 p-3">
-            <div className="flex justify-center items-center"><h4>Qual o nome desta personagem?</h4></div>
-            <Image className="border-2 border-gray-900" src={Yamato} alt=""></Image>
-            <div className="mt-3"><input type="text" placeholder="Put here your answer" className="w-full outline-hidden border-gray-900 border-2" /></div>
-          </div>
-          </div>
+          <div className="flex flex-col bg-white text-gray-900 p-3">
+          <div className="flex justify-center items-center"><h4>Qual o nome desta personagem?</h4></div>
+          <Image className="border-2 border-gray-900" src={Yamato} alt=""></Image>
+          <div className="mt-3"><input type="text" placeholder="Put here your answer" className="w-full outline-hidden border-gray-900 border-2" /></div>
+          <button className="text-white bg-gray-900">Enviar resposta</button>
+        </div>
+        </div></>)
+          : (<>
+          <div className="bg-red-500">Tempo acabou</div>
+          <div className="flex flex-col justify-center mt-6">
+          <div>Quer jogar novamente?</div>
+          <button className="bg-green-500" onClick={() => {handleGameStart()}}>Iniciar jogo</button>
+          </div></>) }
+          
           </>
         )}
       </div>
